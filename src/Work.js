0da755c33131d33738e7c98ba5d9ae6e586f9281
pjcevans <peterjcevans@gmail.com>
2017-11-20 09:31:07 +0000

    added gif to data vis project

--- src/Work.js
+++ src/Work.js
@@ -5,7 +5,7 @@ import "./Work.css";
 import dkpThumb from './images/dkp-thumb.png';
 import todoThumb from './images/todo-thumb.png';
 import dkpSampleData from './images/sampledata-content1.png';
-import dkpComparing from './images/dkpcomparing-content2.png';
+import dkpComparing from './images/dkpcomparing-content2.gif';
 import dkpthunk from './images/thunk-content3.png';
 import githubLogo from "./images/github.svg"
 import herokuLogo from "./images/heroku.svg"
@@ -21,7 +21,7 @@ const modals = [
     secondHeading: "What",
     thirdHeading: "How",
     firstContent: "A community of gamers regularly exported their scores as data but had no means to analyse trends or disparity",
-    secondContent: "The users wanted to be able to compare changes to selectable scores at any point over time, as well as to be able to visually compare current scores for selected users",
+    secondContent: "The users wanted to be able to compare changes to selectable scores at any point over time, as well as to be able to visually compare current scores for selected users. This is achieved by showing the current scores over time by default and showing a diff against the previous export when 'Changes' is selected.",
     thirdContent: "Since data had to be persisted for multiple users over time it made sense to set up a MongoDB solution. Redux was used as a data layer and asynchronous API call outcomes were managed using Thunk.",
     firstImage: dkpSampleData,
     secondImage: dkpComparing,
@@ -38,8 +38,8 @@ const modals = [
     secondHeading: "What",
     thirdHeading: "How",
     firstContent: "I wanted to contribue to the open source learning community which has been so helpful in my own study. An advanced To Do application that showcases full CRUD functionality on the front-end using Redux seemed like an effective manner in which to help other developers to discover Redux.",
-    secondContent: "The intention was to exemplify 'What I wish I had been told' when first starting out with Redux, such as how to use the Chrome DevTools extension, which is invaluable.",
-    thirdContent: "The result is an app showing how actions can be triggered from anywhere in the app and the results are transparent, predictable, and testable.",
+    secondContent: "The intention was to exemplify 'What I wish I had been told' when first starting out with Redux, such as how to use the Chrome DevTools extension, which is invaluable. A To Do app has a clear set of necessary functions which map to an easy to understand set of actions.",
+    thirdContent: "The result is an app showing how actions can be triggered from anywhere in the app and the results are transparent, predictable, and testable. Data is stored in the redux store and persisted in browser cache so that the app is functional regardless of page refreshes but does not have the added complexity of a back end API or database.",
     firstImage: subTodoGif,
     secondImage: reduxTodo,
     thirdImage: stateTodo,
